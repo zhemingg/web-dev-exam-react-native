e49632f71ab756e20c81a4c6b6c27bef45b27316
ZhemingGao <gao19911026@gmail.com>
2018-06-28 15:53:01 -0700

    implemented the preview view of the assignment

--- elements/AssignmentWidget.js
+++ elements/AssignmentWidget.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AssignmentWidgetServiceClient from '../services-client/AssignmentWidgetServiceClient ';
-import {ScrollView, TextInput, Text, StyleSheet} from 'react-native';
-import {FormLabel, FormInput, FormValidationMessage, Button} from 'react-native-elements';
+import {ScrollView, TextInput, Text, StyleSheet, View} from 'react-native';
+import {FormLabel, FormInput, FormValidationMessage, Button, ListItem} from 'react-native-elements';
 
 export default class AssignmentWidget extends React.Component {
     static navigationOptions = {title: 'AssignmentWidget'};
@@ -10,21 +10,24 @@ export default class AssignmentWidget extends React.Component {
         super(props);
         this.state = {
             assignmentId: "",
-            points: '',
-            description: ''
+            points: '0',
+            description: '',
+            preview: false,
+            assignmentName: "New Assignment"
         }
         this.AssignmentWidgetServiceClient = AssignmentWidgetServiceClient.instance;
         this.findAssignmentById = this.findAssignmentById.bind(this);
+        this.viewMode = this.viewMode.bind(this);
     }
 
     componentDidMount() {
-        const {navigation} = this.props;
-        //const topicId = navigation.getParam("topicId")
-        const topicId = "32"
-        this.findAssignmentById(this.state.assignmentId);
-        fetch("http://localhost:8080/api/topic/" + topicId + "/widget")
-            .then(response => (response.json()))
-            .then(widgets => this.setState({widgets}))
+        // const {navigation} = this.props;
+        // //const topicId = navigation.getParam("topicId")
+        // const topicId = "32"
+        // this.findAssignmentById(this.state.assignmentId);
+        // fetch("http://localhost:8080/api/topic/" + topicId + "/widget")
+        //     .then(response => (response.json()))
+        //     .then(widgets => this.setState({widgets}))
     }
 
     findAssignmentById(id) {
@@ -33,27 +36,66 @@ export default class AssignmentWidget extends React.Component {
 
     }
 
+
+    viewMode(isPreview) {
+        if (isPreview) {
+            return (
+                <View>
+                    <Text>Preview</Text>
+                    <Text>{this.state.assignmentName}</Text>
+                    <Text>{this.state.points}</Text>
+                    <Text>{this.state.description}</Text>
+                    <FormLabel>Essay answer</FormLabel>
+                    <FormInput></FormInput>
+
+
+                    <Text>Upload a File</Text>
+                    <Button title="Choose File"/>
+                    <Text>No file chosen</Text>
+
+                    <FormLabel>Submit a link</FormLabel>
+                    <FormInput></FormInput>
+
+                    <Button title="Cancel"/>
+                    <Button title="Submit"/>
+
+
+                    
+
+
+
+                </View>
+            );
+        } else {
+            return (
+                <View>
+                    <FormLabel>Assignment Name</FormLabel>
+                    <FormInput></FormInput>
+
+                    <FormLabel>Assignment Points</FormLabel>
+                    <FormInput></FormInput>
+
+                    <FormLabel>Assignment Description</FormLabel>
+                    <TextInput
+                        multiline={true}
+                        numberOfLines={10}
+                        onChangeText={(description) => this.setState({description})}
+                        value={this.state.description}/>
+                    <Button title="Save"/>
+                </View>
+            );
+        }
+    }
+
     render() {
         return (
-            <ScrollView >
-                <FormLabel>Assignment Name</FormLabel>
-                <FormInput></FormInput>
-
-                <FormLabel>Assignment Points</FormLabel>
-                <FormInput></FormInput>
-
-                <FormLabel>Assignment Description</FormLabel>
-                <TextInput
-                    multiline={true}
-                    numberOfLines={10}
-                    onChangeText={(description) => this.setState({description})}
-                    value={this.state.description}/>
-                <Button title="Save"/>
-
+            <ScrollView>
+                <Button title="Preview"
+                        onPress={() => this.setState({preview: !this.state.preview})}/>
+                {this.viewMode(this.state.preview)}
             </ScrollView>
         )
     }
-
 
 
 }
